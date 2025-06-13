2ac1a423737898fa3a7c954da4af06f295c396f5
Victor Hugo Ferreira Soares <victorsoaresho@gmail.com>
2025-06-12 22:12:25 -0300

    up

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,6 @@ module.exports = new Pool({
   user: 'victorsoaresho',
   password: 'senha',
   host: 'db',
-  port: 8226,
+  port: 5432, // Corrigido de 8226 para 5432
   database: 'launchstore'
 });
