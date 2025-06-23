c333181551d7f37b209e382d94c63ec60d2a3b40
Victor Hugo Ferreira Soares <victorsoaresho@gmail.com>
2025-06-23 19:54:21 -0300

    teste

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 module.exports = new Pool({
   user: 'victorsoaresho',
   password: 'senha',
-  host: 'db',
-  port: 8226, // Corrigido de 8226 para 5432
+  host: 'launchstore_db', 
+  port: 5432, 
   database: 'launchstore'
 });
